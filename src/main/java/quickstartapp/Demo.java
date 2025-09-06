16e011176aafa726eb6598a594ef574b9abb0f58
Ravi Prakash <raviprakashofficial2024@gmail.com>
2025-09-06 10:30:08 +0530

    next commit

---
+++ src/main/java/quickstartapp/Demo.java
@@ -0,0 +1,10 @@
+package quickstartapp;
+
+public class Demo 
+{
+    public main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Started Jenkin Project" )
+    }
+}
